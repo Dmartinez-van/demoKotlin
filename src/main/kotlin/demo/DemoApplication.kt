d361a54c33b21b35e4458fe91aeb37018d54697d
DMartinez-Van <david.antonio.martinez@gmail.com>
2024-03-19 13:58:02 -0700

    add data class Message to fun index

--- src/main/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -3,7 +3,6 @@ package demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
@@ -13,8 +12,15 @@ fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
 
+data class Message(val id: String?, val text: String)
+
 @RestController
 class MessageController {
 	@GetMapping("/")
-	fun index(@RequestParam("name") name: String) = "Hello, $name!"
+	fun index() = listOf(
+		Message("1", "Hello!"),
+		Message("2", "Bonjour!"),
+		Message("3", "Privet!"),
+		Message("4", "Ohayoo Gozaimasu!"),
+	)
 }
